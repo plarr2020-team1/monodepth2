d0faa93d33706082bc41e6833c9b6b84831023e9
Enis Simsar <enisimsar@gmail.com>
2020-06-25 17:40:46 +0300

    (feat): singleton approach for monodepth2

--- monodepth2/infer.py
+++ monodepth2/infer.py
@@ -11,6 +11,10 @@ import os
 import PIL.Image as pil
 import numpy as np
 
+encoder = None
+depth_decoder = None
+feed_height = None
+feed_width = None
 
 def infer_depth(model_name, img):
     """
@@ -30,40 +34,42 @@ def infer_depth(model_name, img):
     Returns: depth_array, disparity_image
     """
 
+    global depth_decoder, encoder, feed_width, feed_height
     if torch.cuda.is_available():
         device = torch.device("cuda")
     else:
         device = torch.device("cpu")
 
-    download_model_if_doesnt_exist(model_name)
-    model_path = os.path.join("models", model_name)
-    print("-> Loading model from ", model_path)
-    encoder_path = os.path.join(model_path, "encoder.pth")
-    depth_decoder_path = os.path.join(model_path, "depth.pth")
+    if encoder is None or depth_decoder is None or feed_width is None or feed_height is None:
+        download_model_if_doesnt_exist(model_name)
+        model_path = os.path.join("models", model_name)
+        print("-> Loading model from ", model_path)
+        encoder_path = os.path.join(model_path, "encoder.pth")
+        depth_decoder_path = os.path.join(model_path, "depth.pth")
 
-    # LOADING PRETRAINED MODEL
-    print("   Loading pretrained encoder")
-    encoder = monodepth2.networks.ResnetEncoder(18, False)
-    loaded_dict_enc = torch.load(encoder_path, map_location=device)
+        # LOADING PRETRAINED MODEL
+        print("   Loading pretrained encoder")
+        encoder = monodepth2.networks.ResnetEncoder(18, False)
+        loaded_dict_enc = torch.load(encoder_path, map_location=device)
 
-    # extract the height and width of image that this model was trained with
-    feed_height = loaded_dict_enc['height']
-    feed_width = loaded_dict_enc['width']
-    filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if
-                         k in encoder.state_dict()}
-    encoder.load_state_dict(filtered_dict_enc)
-    encoder.to(device)
-    encoder.eval()
+        # extract the height and width of image that this model was trained with
+        feed_height = loaded_dict_enc['height']
+        feed_width = loaded_dict_enc['width']
+        filtered_dict_enc = {k: v for k, v in loaded_dict_enc.items() if
+                            k in encoder.state_dict()}
+        encoder.load_state_dict(filtered_dict_enc)
+        encoder.to(device)
+        encoder.eval()
 
-    print("   Loading pretrained decoder")
-    depth_decoder = monodepth2.networks.DepthDecoder(
-        num_ch_enc=encoder.num_ch_enc, scales=range(4))
+        print("   Loading pretrained decoder")
+        depth_decoder = monodepth2.networks.DepthDecoder(
+            num_ch_enc=encoder.num_ch_enc, scales=range(4))
 
-    loaded_dict = torch.load(depth_decoder_path, map_location=device)
-    depth_decoder.load_state_dict(loaded_dict)
+        loaded_dict = torch.load(depth_decoder_path, map_location=device)
+        depth_decoder.load_state_dict(loaded_dict)
 
-    depth_decoder.to(device)
-    depth_decoder.eval()
+        depth_decoder.to(device)
+        depth_decoder.eval()
 
     # Load image and preprocess
     input_image = img.convert('RGB')
